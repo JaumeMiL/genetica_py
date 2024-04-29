efcfbf4858d6d240ffd22a616335383c127c4a32
Jaume Mora <moraladariajaume@gmail.com>
2024-04-29 09:03:42 +0200

    Main començat

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+def main():
+    comanda = input()
+    while comanda != 'fi':
+
+        if comanda == 'experiment':
+            print('experiment')
+            #aquí falta cosa per llegir i llavors cridarem a experiment
+            experiment()
+
+        elif comanda == 'consulta_individu':
+            print('consulta_individu')
+            id_ind = input()
+            consulta_individu(id_ind)
+        
+        elif comanda == 'consulta_tret':
+            print('consulta_tret')
+            nom_tret = input()
+            consulta_tret(nom_tret)
+
+        elif comanda == 'distribucio_tret':
+            print('distribucio_tret')
+            nom_tret = input()
+            distribucio_tret(nom_tret)
+
+        elif comanda == 'afegir_tret':
+            print('afegir_tret')
+            nom_tret = input()
+            afegir_tret(nom_tret)
+        
+        comanda = input()
