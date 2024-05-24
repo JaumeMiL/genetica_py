a4fcc501854357f799d806d2d988b65717f36a4b
PAbella8 <pabellaescudero@gmail.com>
2024-05-24 17:52:46 +0200

    Primera modificación conjunts

--- conjunt_individus.py
+++ conjunt_individus.py
@@ -1,10 +1,27 @@
 from arbre_binari import Node
 from individu import Individu
+from pytokr import pytokr
+
+item, items = pytokr(iter = True)
 
 class ConjuntIndividus:
+    def __init__(self,num_ind,num_gen):
         self._list_ind = [None for _ in range(num_ind + 1)]
+        for i in range(num_ind):
+            cromosoma = item()
+            self._list_ind()[i] = Individu(cromosoma,num_gen)
         self.individus = {}  # diccionari amb tots els individus
         self.arbre_genealogic = None
+
+    def list_ind(self):
+        return self._list_ind
+    
+    def ind(self,individu):
+        lst = self.list_ind()
+        return lst[individu]
+    
+    def consulta_individu(self, individu):
+        return self.ind(individu).consulta_individu()
 
     def inicialitzar_individus(self, preordre):
         preordre_list = list(preordre)  # Converteixo a llista els elements del preordre
